c1c33836d1006e1e92c26ff53f0324fa7808c156
trikitrok <trikitrok@gmail.com>
2016-02-24 13:56:57 +0100

    Using macro to create all hi-queen functions

--- src/hi_queen/core.clj
+++ src/hi_queen/core.clj
@@ -1,19 +1,15 @@
 (ns hi-queen.core)
 
+(defn hi-queen [introduction]
+  (str introduction ", so please your Majesty"))
+
 (defmacro def-hi-queen [name intro-formula]
   (list 'defn
         (symbol (str (clojure.string/lower-case (clojure.string/replace name #"\s" "-")) "-hi-queen"))
         []
         (list 'hi-queen (str intro-formula " " name))))
 
-(defn hi-queen [introduction]
-  (str introduction ", so please your Majesty"))
-
 (def-hi-queen "Alice" "My name is")
 (def-hi-queen "March Hare" "My name is")
-
-(defn white-rabbit-hi-queen []
-  (hi-queen "I'm the White Rabbit"))
-
-(defn mad-hatter-hi-queen []
-  (hi-queen "I'm the Mad Hatter"))
+(def-hi-queen "White Rabbit" "I'm the")
+(def-hi-queen "Mad Hatter" "I'm the")
