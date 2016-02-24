7dc3ec98c07a6634141524bde462f74bef246e7f
trikitrok <trikitrok@gmail.com>
2016-02-24 13:51:11 +0100

    Using a macro to define alice-hi-queen function

--- src/hi_queen/core.clj
+++ src/hi_queen/core.clj
@@ -1,10 +1,15 @@
 (ns hi-queen.core)
 
+(defmacro def-hi-queen [name intro-formula]
+  (list 'defn
+        (symbol (str (clojure.string/lower-case name) "-hi-queen"))
+        []
+        (list 'hi-queen (str intro-formula " " name))))
+
 (defn hi-queen [introduction]
   (str introduction ", so please your Majesty"))
 
-(defn alice-hi-queen []
-  (hi-queen "My name is Alice"))
+(def-hi-queen "Alice" "My name is")
 
 (defn march-hare-hi-queen []
   (hi-queen "My name is March Hare"))
